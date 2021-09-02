c9290dccf60d7a503f9957b9e78fd59efebee22d
cherishxyun <81494787+cherishxyun@users.noreply.github.com>
2021-09-03 06:48:12 +0900

    Update GamePage.js 2021/09/03
    boardGame에서 tagger와 card 추가 
    participant에서 class/id 결정 보류

--- src/Pages/GamePage.js
+++ src/Pages/GamePage.js
@@ -4,7 +4,42 @@ const GamePage = () => {
   
   return (
     <div className="gamePage__container">
-      <div className="gamePage__wrapper">
+      <div className="gamePage__wrapper">import React from 'react';
+
+const GamePage = () => {
+  
+  return (
+    <div id="gamePage__container">
+      <div id="gamePage__wrapper">
+        <div id="round">
+        </div>
+        <div id="score">
+        </div>
+        <section id="boardGame">
+          <div id="participants">
+            <div className="participant"></div>
+          </div>
+          <span id="tagger"></span>
+          <span id="card"></span>
+        </section>
+        <div>
+          <input id="hintOrAnswer"
+          ></input>
+        </div>
+        <div id="timeLimit">
+        </div>
+        <div>
+          <textarea id="chattingDisplay"
+          ></textarea>
+          <input id="chattingInput"
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage
         <div className="round">
         </div>
         <div className="score">
