3c5bb7f6fa2117dd662894b002832f064df08bb3
cherishxyun <81494787+cherishxyun@users.noreply.github.com>
2021-09-03 07:06:37 +0900

    Components File Upload 2021/09/03
    Tagger는 틀만 제작.
    Participant는 명수에 따라 박스 안에 닉네임이 들어갈 수 있도록 map함수 활용. 
    현재 사용자 관련 정보를 서버로 불러오는 방법 없기 떄문에, Dummy Data(dummyParticipant)가 있다는 가정 하에 코드 작성.

---
+++ src/Components/Participant.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const nickname = dummyParticipant.map(el => {
+  return el.nickname; //dummyParticipant에서 닉네임 가져오기
+})
+
+return (
+  <div className="participant">
+    <div className="participant__nickname">{nickname}</div> /*블럭에 닉네임 입력*/
+  </div>
+)
+
+
+// dummyData 만들어줘야 함
---
+++ src/Components/Tagger.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+//dummyParticipant에 있는 데이터를 가지고 와서 술래 정보 입력
+
+return 
+
+
