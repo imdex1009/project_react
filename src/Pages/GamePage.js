77195b94a9645a30b758fddbf9f53ffdb9ce41ee
cherishxyun <81494787+cherishxyun@users.noreply.github.com>
2021-09-05 11:36:43 +0900

    Update GamePage.js
    주석변경

--- src/Pages/GamePage.js
+++ src/Pages/GamePage.js
@@ -5,25 +5,34 @@ const GamePage = () => {
   return (
     <div id="gamePage__container">
       <div id="gamePage__wrapper">
-        <div id="round"> /* 게임 끝나면 round 올려주기 */
+        <div id="round"> 
+        {/* 게임 끝나면 round 올려주기 */}
         </div>
-        <div id="score"> /* 게임 결과에 따라 점수 변동 */
+        <div id="score"> 
+        {/* 게임 결과에 따라 점수 변동 */}
         </div>
         <section id="boardGame">
           <div id="participants"> 
-            <div className="participant"></div> /* 참가자마다의 컴포넌트 */
+            <div className="participant"></div> 
+            {/* 참가자마다의 컴포넌트  */}
           </div>
-          <span id="tagger"></span> /* 술래 */
-          <span id="card"></span> /* 단어가 들어가있는 13개의 카드, 선택 가능하도록 */
+          <div id="tagger"></div> 
+          {/* 술래 */}
+          <div id="card"></div> 
+          {/* 단어가 들어가있는 13개의 카드, 선택 가능하도록  */}
         </section>
         <div>
-          <input id="hintOrAnswer"></input> /* 힌트 또는 답 입력하기 */
+          <input id="hintOrAnswer"></input> 
+          {/* 힌트 또는 답 입력하기  */}
         </div>
-        <div id="timeLimit"> /* 시간제한 */
+        <div id="timeLimit"> 
+        {/* 시간제한  */}
         </div>
-        <div>
-          <textarea id="chattingDisplay"></textarea> /* 채팅 내용 보여지는 부분 */
-          <input id="chattingInput"></input> /* 채팅 입력하는 칸 */
+        <div id="chatting">
+          <textarea id="chattingDisplay"></textarea> 
+          {/* 채팅 내용 보여지는 부분  */}
+          <input id="chattingInput"></input> 
+          {/* 채팅 입력하는 칸  */}
         </div>
       </div>
     </div>
